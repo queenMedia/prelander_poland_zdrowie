1490cd43b4a516f590a802632c02abfca42ae8e5
gomarsergio <sergio@callytek.com>
2023-08-25 17:40:33 -0600

    🐛 fix: safari incompatibility

--- src/logic/data/bucketAssets.ts
+++ src/logic/data/bucketAssets.ts
@@ -1,37 +1,32 @@
 import config from "@config";
-import { getConfig } from "@services/bucket";
+import { bucketConfigStore, bucketImagesStore } from "@stores";
 
 const character_snake_case =  new URLSearchParams(window.location.search).get("character")
 const offer_snake_case =  new URLSearchParams(window.location.search).get("offer")
 
-const getDataBucket = async () => {
-    return await getConfig(character_snake_case || "");
-}
+let bucketConfig: any;
+bucketConfigStore.subscribe((value) => {
+    bucketConfig = value;
 
-const bucketConfig = await getDataBucket();
+    const url_bucket = config.url_bucket;
+    const language = (bucketConfig !== 0 && bucketConfig !== null) ? bucketConfig.contentKeys.language : null;
+    const currencyCode = (bucketConfig !== 0 && bucketConfig !== null) ? bucketConfig.targetCurrency.code : null;
+    const countryCode = (bucketConfig !== 0 && bucketConfig !== null) ? bucketConfig.targetKeys.countryCode : null;
 
-const url_bucket = config.url_bucket;
-const language = (bucketConfig !== 0) ? bucketConfig.contentKeys.language : null;
-const currencyCode = (bucketConfig !== 0) ? bucketConfig.targetCurrency.code : null;
-const countryCode = (bucketConfig !== 0) ? bucketConfig.targetKeys.countryCode : null;
-
-
-/* bucket images */
-export const Img1 = `${url_bucket}/sharing/characters/${character_snake_case}/img1.jpg`;
-export const Img2 = `${url_bucket}/sharing/characters/${character_snake_case}/img2.jpg`;
-export const Img3 = `${url_bucket}/sharing/characters/${character_snake_case}/img3.jpg`;
-export const TableImg = `${url_bucket}/${offer_snake_case}/tables/${language}/${currencyCode}.jpg`;
-
-export const TopBannerImg = `${url_bucket}/sharing/banners/${countryCode}.jpg`;
-
-export const BillGatesImg = `${url_bucket}/sharing/billgates/${offer_snake_case}.jpg`;
-export const FBPostImg = `${url_bucket}/sharing/posts/${countryCode}.jpg`;
-export const GIFImg = `${url_bucket}/sharing/gifs/${language}/${currencyCode}.jpg`;
-export const FamilyImg = `${url_bucket}/sharing/family/${currencyCode}.jpg`;
-
-export const CheckImg = `${url_bucket}/sharing/checks/${countryCode}.jpg`;
-export const OfferBannerImg = `${url_bucket}/${offer_snake_case}/banners/${countryCode}.jpg`;
-export const Step2Img = `${url_bucket}/sharing/steps/${language}/${currencyCode}/2.jpg`;
-export const Step3Img = `${url_bucket}/sharing/steps/${language}/${currencyCode}/3.jpg`;
-
-export const sideBarFormImg  = `${url_bucket}/${offer_snake_case}/forms/${countryCode}.jpg`;
+    bucketImagesStore.set({
+        Img1: `${url_bucket}/sharing/characters/${character_snake_case}/img1.jpg`,
+        Img2: `${url_bucket}/sharing/characters/${character_snake_case}/img2.jpg`,
+        Img3: `${url_bucket}/sharing/characters/${character_snake_case}/img3.jpg`,
+        TableImg: `${url_bucket}/${offer_snake_case}/tables/${language}/${currencyCode}.jpg`,
+        TopBannerImg: `${url_bucket}/sharing/banners/${countryCode}.jpg`,
+        BillGatesImg: `${url_bucket}/sharing/billgates/${offer_snake_case}.jpg`,
+        FBPostImg: `${url_bucket}/sharing/posts/${countryCode}.jpg`,
+        GIFImg: `${url_bucket}/sharing/gifs/${language}/${currencyCode}.jpg`,
+        FamilyImg: `${url_bucket}/sharing/family/${currencyCode}.jpg`,
+        CheckImg: `${url_bucket}/sharing/checks/${countryCode}.jpg`,
+        OfferBannerImg: `${url_bucket}/${offer_snake_case}/banners/${countryCode}.jpg`,
+        Step2Img: `${url_bucket}/sharing/steps/${language}/${currencyCode}/2.jpg`,
+        Step3Img: `${url_bucket}/sharing/steps/${language}/${currencyCode}/3.jpg`,
+        sideBarFormImg: `${url_bucket}/${offer_snake_case}/forms/${countryCode}.jpg`,
+    });
+});
